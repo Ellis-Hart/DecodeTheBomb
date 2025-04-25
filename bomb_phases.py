f98b20e0ae37a80423e90396197842702065bca1
Ellis-Hart <ellis.hart06@gmail.com>
2025-04-25 16:34:13 -0400

    Win/Loss sound effects

--- bomb_phases.py
+++ bomb_phases.py
@@ -99,9 +99,15 @@ class Lcd(Frame):
         # reconfigure the GUI
         
         if success:
-            self._lmessage = tkinter.Label(self, text="You Defused the Bomb!", font=("Courier New", 24), fg="green")
+            self._lmessage = tkinter.Label(self, text="You defused the Bomb!", font=("Courier New", 24), fg="green")
             self._lmessage.grid(row=0, column=1, pady=50)
-        
+            win_sound = pygame.mixer.Sound("win.wav")
+            win_sound.play()
+        else:
+            self._lmessage = tkinter.Label(self, text="You exploded!", font=("Courier New", 24), fg="green")
+            self._lmessage.grid(row=0, column=1, pady=50)
+            loss_sound = pygame.mixer.Sound("loss.wav")
+            loss_sound.play()
         
         # the retry button
         self._bretry = tkinter.Button(self, bg="red", fg="white", font=("Courier New", 18), text="Retry", anchor=CENTER, command=self.retry)
